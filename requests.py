91cff74273b3c80d9d62c83cd6fc8a406242c8d3
Snehil Seenu <80764244+seenusnehil@users.noreply.github.com>
2022-12-10 01:08:09 +0530

    Delete requests.py

--- requests.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "https://api.uberduck.ai/reference-audio"
-
-headers = {"accept": "application/json"}
-
-response = requests.get(url, headers=headers)
-
-print(response.text)
