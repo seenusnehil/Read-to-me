218bf2bf5a43daa740f31a0f7a4b43c6c7bb342d
Namrata Vichare <nav272@nyu.edu>
2022-12-12 00:46:41 +0530

    Update get-request.py

--- get-request.py
+++ get-request.py
@@ -7,16 +7,19 @@ url = "https://api.uberduck.ai/speak"
 payload = {
     "voice": "lj",
     "pace": 1,
-    "speech": """Nadezhda Alliluyeva (1901–1932), also known as Nadya or Nadia, was the second wife of Soviet leader 
-    Joseph Stalin. She was born in Baku to a friend of Stalin, a fellow revolutionary, and was raised in Saint 
-    . Alliluyeva was exposed to revolutionary activity throughout her youth. Having known Stalin from a young age, 
-    she married him when she was 18, and they had two children. Alliluyeva worked as a secretary for Bolshevik leaders, 
-    including Vladimir Lenin and Stalin, and also as an assistant in the Department of Agitation and Propaganda, before 
-    enrolling at the Industrial Academy in Moscow to study synthetic fibres and become an engineer. She had health 
-    issues, which had an adverse impact on her relationship with Stalin. She also suspected he was unfaithful, 
-    which led to frequent arguments with him. On several occasions, Alliluyeva reportedly contemplated leaving Stalin. 
-    After an argument she shot herself early in the morning of 9 November 1932. (Full article...)
-    
+    "speech": """
+    You and I experience life differently
+Is the idea of immortality and rebirth real or conceptual?
+What is real? Can the conceptual be real? Our
+understanding of reality is a function of the capabilities
+of our body or deha, home of the atma. What is real for a
+plant is not real for an animal, what is real for one human
+is not real for another. Nature is all about diversity. This
+understanding of the body, the instrument through
+which we experience and express reality, is not explicitly
+a part of The Gita, but certainly a part of Vedic knowledge
+elaborated in the Upanishads, that Krishna assumes
+Arjuna to be familiar with.
     """
 }
 headers = {
