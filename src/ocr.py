2f6f7ddb2b7a0563c170f8d287bc874159c050bf
Namrata Vichare <nav272@nyu.edu>
2022-12-12 00:48:11 +0530

    Update ocr.py

--- src/ocr.py
+++ src/ocr.py
@@ -11,25 +11,15 @@ def detect_text(path):
     import io
     client = vision.ImageAnnotatorClient()
 
-    # with io.open(path, 'rb') as image_file:
     with io.open(path, 'rb') as image_file:
         content = image_file.read()
-    # content = img
 
     image = vision.Image(content=content)
 
     response = client.text_detection(image=image)
     texts = response.text_annotations
-    #print('Texts:')
 
     output.append(texts[0].description)
-    #for text in texts:
-     #   print('\n"{}"'.format(text.description))
-
-        #vertices = (['({},{})'.format(vertex.x, vertex.y)
-          #          for vertex in text.bounding_poly.vertices])
-
-        #print('bounds: {}'.format(','.join(vertices)))
 
     if response.error.message:
         raise Exception(
